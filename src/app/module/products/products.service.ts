e802c90a36895072b668215253fa0774860158f7
sumonpaul55 <sumpaul100@gmail.com>
2024-07-13 01:53:02 +0600

    product upload properly increament withh validationn and quantity

--- src/app/module/products/products.service.ts
+++ src/app/module/products/products.service.ts
@@ -2,6 +2,12 @@ import { TProducts } from "./products.interface";
 import { Products } from "./products.model";
 
 const addProductDb = async (payLoad: TProducts) => {
+  // isExist product
+  const isExistProduct = await Products.findOne({ name: payLoad.name });
+  if (isExistProduct) {
+    const result = await Products.findOneAndUpdate({ name: payLoad.name }, { $inc: { quantity: payLoad.quantity } }, { new: true });
+    return result;
+  }
   const result = await Products.create(payLoad);
   return result;
 };
--- src/app/module/products/products.validation.ts
+++ src/app/module/products/products.validation.ts
@@ -4,7 +4,7 @@ const addProductValidationSchema = z.object({
   image: z.string().url(), // Assuming the image is a URL
   name: z.string().min(1, "Name is required"),
   brand: z.string().min(1, "Brand is required"),
-  quantity: z.number().int().nonnegative("Quantity must be a non-negative integer"),
+  quantity: z.number().int().positive("Quantity must be a non-negative integer"),
   price: z.number().nonnegative("Price must be a non-negative number"),
   rating: z.number().min(0).max(5, "Rating must be between 0 and 5"),
   description: z.string().min(1, "Description is required"),
