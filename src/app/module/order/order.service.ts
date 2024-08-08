d01a2f57192bc0fc24b16c6051285dbb5e33eb39
sumonpaul55 <sumpaul100@gmail.com>
2024-08-09 01:38:34 +0600

    order checkout qunatity reduce after confirm chekcout successfull using start sesion transition and working fine

--- src/app/module/order/order.service.ts
+++ src/app/module/order/order.service.ts
@@ -1,9 +1,37 @@
+import { startSession } from "mongoose";
 import { TOrder } from "./order.interface";
 import { Order } from "./order.model";
+import { Products } from "../products/products.model";
+import httpStatus from "http-status";
 
 const addOrderDb = async (payload: TOrder) => {
-  const result = await Order.create(payload);
-  return result;
+  // reduce qunatity from product after confirm chekcout
+  const orderedProduct = payload.products;
+  const session = await startSession();
+  try {
+    session.startTransaction();
+
+    const updatePromises = orderedProduct.map(async (product) => {
+      //find the produc is exist
+      const existProduct = await Products.findById(product.productId).session(session);
+      //  product exist and have enough quantity
+      if (!existProduct || existProduct.availableQuantity < product.productQuantity) {
+        throw new AppError(httpStatus.INTERNAL_SERVER_ERROR, "Product Not available or Insufficient qunatity");
+      }
+      // reduce qunatity from main produt after confirem
+      existProduct.availableQuantity -= product.productQuantity;
+
+      await existProduct.save({ session });
+    });
+    await Promise.all(updatePromises);
+    const result = await Order.create(payload);
+    await session.commitTransaction();
+    return result;
+  } catch (error) {
+    await session.abortTransaction();
+    await session.endSession();
+    throw new AppError(httpStatus.INTERNAL_SERVER_ERROR, "Internal Server error order not completed");
+  }
 };
 
 export const orderService = { addOrderDb };
