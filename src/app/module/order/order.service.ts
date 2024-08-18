8801e8e8d5390640d77c5c08bd602f6d7564583c
sumonpaul55 <sumpaul100@gmail.com>
2024-08-18 16:42:05 +0600

    all transition and rollback removed for clear error for checkout

--- src/app/module/order/order.service.ts
+++ src/app/module/order/order.service.ts
@@ -7,32 +7,22 @@ import httpStatus from "http-status";
 const addOrderDb = async (payload: TOrder) => {
   // reduce qunatity from product after confirm chekcout
   const orderedProduct = payload.products;
-  const session = await startSession();
-  try {
-    session.startTransaction();
-
-    const updatePromises = orderedProduct.map(async (product) => {
-      //find the produc is exist
-      const existProduct = await Products.findById(product.productId).session(session);
-      //  product exist and have enough quantity
-      if (!existProduct || existProduct.availableQuantity < product.productQuantity) {
-        throw new AppError(httpStatus.INTERNAL_SERVER_ERROR, "Product Not available or Insufficient qunatity");
-      }
-      // reduce qunatity from main produt after confirem
-      existProduct.availableQuantity -= product.productQuantity;
-      await existProduct.save({ session });
-    });
-    // ensure all asynchrounus operation completed
-    await Promise.all(updatePromises);
-    const result = await Order.create(payload);
-    await session.commitTransaction();
-    await session.endSession();
-    return result;
-  } catch (error) {
-    await session.abortTransaction();
-    await session.endSession();
-    throw new AppError(httpStatus.INTERNAL_SERVER_ERROR, "Internal Server error order not completed");
-  }
+  // const session = await startSession();
+  const updatePromises = orderedProduct.map(async (product) => {
+    //find the produc is exist
+    const existProduct = await Products.findById(product.productId);
+    //  product exist and have enough quantity
+    if (!existProduct || existProduct.availableQuantity < product.productQuantity) {
+      throw new AppError(httpStatus.INTERNAL_SERVER_ERROR, "Product Not available or Insufficient qunatity");
+    }
+    // reduce qunatity from main produt after confirem
+    existProduct.availableQuantity -= product.productQuantity;
+    await existProduct.save();
+  });
+  // ensure all asynchrounus operation completed
+  await Promise.all(updatePromises);
+  const result = await Order.create(payload);
+  return result;
 };
 
 export const orderService = { addOrderDb };
